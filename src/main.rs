e1dc7abff84585c71478fb746171015fbae7586b
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-04 17:21:59 +0900

    implement post-02

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std]
 #![no_main]
 
+extern crate rlibc;
+
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -10,5 +14,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
