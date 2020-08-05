0ce56fa70fbd3542e13e636bb9171094faf8f7f5
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-05 11:48:25 +0900

    post-03をすべて実装

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-
+    // panic!("Some pani message");
     loop {}
 }
