6117e63b9078f9bb7199b67ef663ed66ba1c1401
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-07 10:43:12 +0900

    lib.rs導入による統合テストを実装

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,35 +5,22 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-extern crate rlibc;
-
 use core::panic::PanicInfo;
 use blog_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info);
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
