f42ec04b8b06ae5c675ecd4e1e07df39f0ba52f1
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-15 09:40:54 +0900

    post-06: ダブルフォールトハンドラの設定まで

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,9 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    // ブレークポイント例外を発生させる
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    }
 
     #[cfg(test)]
     test_main();
