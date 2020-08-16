58d4c24782e37043caf0b893f13fdd1b68c82402
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-08-16 18:09:40 +0900

    post-08: ページング入門をすべて実装

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,10 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    let ptr = 0x2047d2 as *mut u32;
+    use x86_64::registers::control::Cr3;
 
-    unsafe { let x = *ptr; }
-    println!("read worked");
-
-    unsafe { *ptr = 42; }
-    println!("write worked");
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
